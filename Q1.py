bcbe9e2990f696b0d4455fcd932950927206fa5b
dtonda8 <davidbatonda7@gmail.com>
2024-08-27 18:40:31 +1000

    Add sols

--- Q1.py
+++ Q1.py
@@ -1,6 +1,20 @@
-def max_nesting(s: str) -> int:
-	raise NotImplementedError()
+from data_structures.linked_stack import LinkedStack
 
+def max_nesting(s: str) -> int:
+	open_brk_st = LinkedStack()
+
+	current_nesting = 0
+	max_nesting = 0
+	for ch in s:
+		if ch == "(":
+			open_brk_st.push(ch)
+			current_nesting += 1
+			max_nesting = max(max_nesting, current_nesting)
+		elif ch == ")":
+			current_nesting -= 1
+			open_brk_st.pop()
+
+	return max_nesting
 
 if __name__ == "__main__": 
 	pass
--- Q2.py
+++ Q2.py
@@ -1,9 +1,16 @@
-from typing import Iterable
+from typing import List, Iterable
 from data_structures.array_sorted_list import ArraySortedList
 from book import Book
 
 def get_books(books: ArraySortedList[Book], start_year: int) -> Iterable[str]:
-	raise NotImplementedError()
+	# Get index. Remember: assignments prohibit you from accessing private members/methods (i.e. those that are prefix with '_')
+	dummy = Book("", start_year, "")
+	books.add(dummy)
+	idx = books.index(dummy)
+	books.remove(dummy) # remove dummy added
+
+	for i in range(idx, len(books)):
+		yield books[i].publication_year
 
 
 if __name__ == "__main__":
